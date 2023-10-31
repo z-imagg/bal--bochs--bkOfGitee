5523c7fc0b6a915804bfed06a6611e66623ce12d
prgrmz01@163.com <prgrmz01@163.com>
2023-11-01 01:13:08 +0800

    line_filter.py开发中: 过滤+映射（到目前为止，运行时过滤器脚本是从环境变量读取来了，需要多一层''，且不能写显示的'\n')

--- makefile_util/line_filter.py
+++ makefile_util/line_filter.py
@@ -6,17 +6,34 @@ _LnFeed_RN="\r\n"
 _LnFeed_N="\n"
 
 #此过滤器 基本能过滤掉大部分非关心目标
-#set filter_expr="def lnFlt(lnK,prevLn): return lnK.__contains__(':') and  not lnK.__contains__(':=') and  not lnK.__contains__('%.') and  not lnK.__contains__('%::') and  not lnK.__contains__('(%):') and  not lnK.__contains__(' = ') and not lnK.startswith('#') and not lnK.split(':')[0].endswith('.h') and not lnK.split(':')[0].endswith('.c')  and not lnK.split(':')[0].endswith('.cc') and not lnK.split(':')[0].endswith('.cpp')  and prevLn not in [f'# Not a target:{_LnFeed_N}'  , f'# Not a target:{_LnFeed_RN}' ] " & python bochs\makefile_util\line_filter.py   cpu.txt cpuo.txt & more cpuo.txt
+#set filter_expr="def lnFltMap(lnK,prevLn): keep= lnK.__contains__(':') and  not lnK.__contains__(':=') and  not lnK.__contains__('%.') and  not lnK.__contains__('%::') and  not lnK.__contains__('(%):') and  not lnK.__contains__(' = ') and not lnK.startswith('#') and not lnK.split(':')[0].endswith('.h') and not lnK.split(':')[0].endswith('.c')  and not lnK.split(':')[0].endswith('.cc') and not lnK.split(':')[0].endswith('.cpp')  and prevLn not in [f'# Not a target:{_LnFeed_N}'  , f'# Not a target:{_LnFeed_RN}' ] ; return (True,f'''{lnK.split(':')[0]}{_LnFeed_N}''') if keep else (False,lnK) " & python bochs\makefile_util\line_filter.py   cpu.txt cpuo.txt & more cpuo.txt
 
-usage_ms_windows=""" 用法: set filter_expr='def lnFlt(lnK,prevLn): return lnK.contains(":")' & python line_filter.py 输入文本文件 输出文本文件 'def lnFlt(lnK,prevLn): return lnK.__contains__(":")' """ #用函数样式
-# usage_ms_windows="""用法: set filter_expr='def lnFlt(lnK,prevLn): return lnK.contains(":")' & python line_filter.py 输入文本文件 输出文本文件 'lnFlt=lambda x:x[0].__contains__(":")' """ #暂时不用lambda样式
+
+#lnFltMap例子: makefile目标过滤器
+def lnFltMap_demo_Makefile_target(lnK,prevLn): 
+	keep= lnK.__contains__(':') \
+	and  not lnK.__contains__(':=') \
+	and  not lnK.__contains__('%.') \
+	and  not lnK.__contains__('%::') \
+	and  not lnK.__contains__('(%):') \
+	and  not lnK.__contains__(' = ') \
+	and not lnK.startswith('#') \
+	and not lnK.split(':')[0].endswith('.h') \
+	and not lnK.split(':')[0].endswith('.c')  \
+	and not lnK.split(':')[0].endswith('.cc') \
+	and not lnK.split(':')[0].endswith('.cpp')  \
+	and prevLn not in [f'# Not a target:{_LnFeed_N}'  , f'# Not a target:{_LnFeed_RN}' ]
+	return (keep,lnK.split(':')[0])
+
+usage_ms_windows=""" 用法: set filter_expr='def lnFltMap(lnK,prevLn): return lnK.contains(":")' & python line_filter.py 输入文本文件 输出文本文件 'def lnFltMap(lnK,prevLn): return lnK.__contains__(":")' """ #用函数样式
+# usage_ms_windows="""用法: set filter_expr='def lnFltMap(lnK,prevLn): return lnK.contains(":")' & python line_filter.py 输入文本文件 输出文本文件 'lnFltMap=lambda x:x[0].__contains__(":")' """ #暂时不用lambda样式
 
 print(f"sys.argv:{sys.argv} ")
 assert os.environ.__contains__('filter_expr') and len(sys.argv)>2, usage_ms_windows
 fn_in=sys.argv[1]
 fn_out=sys.argv[2]
 _filter_expr=os.environ['filter_expr']
-filter_expr=eval(_filter_expr)#去掉一层引号 从 '"def lnFlt2(lnK,prevLn): xxx" ' 变成  "def lnFlt2(lnK,prevLn): xxx"
+filter_expr=eval(_filter_expr)#去掉一层引号 从 '"def lnFltMap2(lnK,prevLn): xxx" ' 变成  "def lnFltMap2(lnK,prevLn): xxx"
 print(f" os.environ['filter_expr']={filter_expr},{type(filter_expr)}")
 
 
@@ -24,20 +41,21 @@ print(f" os.environ['filter_expr']={filter_expr},{type(filter_expr)}")
 filter_func=compile(filter_expr,"<string>","exec")
 print(f" filter_func={filter_func},{type(filter_func)},{filter_func.co_consts}")
 # pdb.set_trace()
-lnFlt=FunctionType(filter_func.co_consts[0],globals(),"lnFlt")
+lnFltMap=FunctionType(filter_func.co_consts[0],globals(),"lnFltMap")
 
-print(lnFlt,lnFlt("a:b","c"))
-#用函数样式: def lnFlt(lnK,prevLn): return lnK.contains(":")
-#暂时不用lambda样式: lambda lnFlt x: x[0].contains(":")
+print(lnFltMap,lnFltMap("a:b","c"))
+#用函数样式: def lnFltMap(lnK,prevLn): return lnK.contains(":")
+#暂时不用lambda样式: lambda lnFltMap x: x[0].contains(":")
 with open(fn_in, "r") as fin:
     lnLs= fin.readlines()
 
 outLnLs=[]
 for k,lnK in enumerate(lnLs):
 	prevLn='' if k==0 else lnLs[k-1]
-	# if lnFlt( (lnK,prevLn) ): #这里是lambda样式, 暂时不用
-	if lnFlt( lnK,prevLn ):   #这里是函数样式, 暂时用此
-		outLnLs.append(lnK)
+	#   lnFltMap( (lnK,prevLn) ): #这里是lambda样式, 暂时不用
+	keep,lnKMapped=lnFltMap( lnK,prevLn ) #这里是函数样式, 暂时用此
+	if keep:   
+		outLnLs.append(lnKMapped)
 		# outLnLs.append(f"[{len(lnK)},{lnK}]")#调试用
 
 outText="".join(outLnLs)
