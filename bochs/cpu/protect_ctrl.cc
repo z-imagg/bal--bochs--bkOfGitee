1871e377d237e70380c7256ee569c7f610b21212
prgrmz01@163.com <prgrmz01@163.com>
2023-11-05 21:39:01 +0800

    日志 LLDT_Ew LTR_Ew LGDT_Ms：记录cpu_mode值

--- bochs/cpu/protect_ctrl.cc
+++ bochs/cpu/protect_ctrl.cc
@@ -471,7 +471,7 @@ void BX_CPP_AttrRegparmN(1) BX_CPU_C::LLDT_Ew(bxInstruction_c *i)
 
   Bit16u _CS_selector_value=BX_CPU_THIS_PTR sregs[BX_SEG_REG_CS].selector.value;
   Bit16u _CS_selector_index=BX_CPU_THIS_PTR sregs[BX_SEG_REG_CS].selector.index;
-  BX_INFO(("记录日志;模拟指令LLDT;LLDT_Ew;此行在区;此行内容;_CS_selector_value:0x%x,_CS_selector_index:0x%x,EIP:0x%x,raw_selector:0x%x,selector:0x%x,descriptor:0x%x;", _CS_selector_value, _CS_selector_index, EIP, raw_selector,selector,descriptor));
+  BX_INFO(("记录日志;模拟指令LLDT;LLDT_Ew;此行在区;此行内容;cpu_mode:%d,_CS_selector_value:0x%x,_CS_selector_index:0x%x,EIP:0x%x,raw_selector:0x%x,selector:0x%x,descriptor:0x%x;", BX_CPU_THIS_PTR cpu_mode,_CS_selector_value, _CS_selector_index, EIP, raw_selector,selector,descriptor));
 
   BX_NEXT_INSTR(i);
 }
@@ -596,7 +596,7 @@ void BX_CPP_AttrRegparmN(1) BX_CPU_C::LTR_Ew(bxInstruction_c *i)
 
   Bit16u _CS_selector_value=BX_CPU_THIS_PTR sregs[BX_SEG_REG_CS].selector.value;
   Bit16u _CS_selector_index=BX_CPU_THIS_PTR sregs[BX_SEG_REG_CS].selector.index;
-  BX_INFO(("记录日志;模拟指令LTR;LTR_Ew;此行在区;此行内容;_CS_selector_value:0x%x,_CS_selector_index:0x%x,EIP:0x%x,raw_selector:0x%x,selector:0x%x,descriptor:0x%x;",_CS_selector_value, _CS_selector_index, EIP, raw_selector, selector, descriptor));
+  BX_INFO(("记录日志;模拟指令LTR;LTR_Ew;此行在区;此行内容;cpu_mode:%d,_CS_selector_value:0x%x,_CS_selector_index:0x%x,EIP:0x%x,raw_selector:0x%x,selector:0x%x,descriptor:0x%x;",BX_CPU_THIS_PTR cpu_mode,_CS_selector_value, _CS_selector_index, EIP, raw_selector, selector, descriptor));
 
 
   BX_NEXT_INSTR(i);
@@ -870,7 +870,7 @@ void BX_CPP_AttrRegparmN(1) BX_CPU_C::LGDT_Ms(bxInstruction_c *i)//模拟 指令
   
   Bit16u _CS_selector_value=BX_CPU_THIS_PTR sregs[BX_SEG_REG_CS].selector.value;
   Bit16u _CS_selector_index=BX_CPU_THIS_PTR sregs[BX_SEG_REG_CS].selector.index;
-  BX_INFO(("记录日志;模拟指令LGDT;LGDT_Ms;此行在区;此行内容;_CS_selector_value:0x%x,_CS_selector_index:0x%x,EIP:0x%x,eaddr:0x%x,base_32:0x%x,limit_16:0x%x;",_CS_selector_value, _CS_selector_index, EIP,eaddr,base_32, limit_16));
+  BX_INFO(("记录日志;模拟指令LGDT;LGDT_Ms;此行在区;此行内容;cpu_mode:%d,_CS_selector_value:0x%x,_CS_selector_index:0x%x,EIP:0x%x,eaddr:0x%x,base_32:0x%x,limit_16:0x%x;",BX_CPU_THIS_PTR cpu_mode,_CS_selector_value, _CS_selector_index, EIP,eaddr,base_32, limit_16));
 
 
   BX_NEXT_INSTR(i);
