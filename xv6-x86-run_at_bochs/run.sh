86f0da0599a92a74c40cbe9cb7fb97082d6a0ede
prgrmz07 <prgrmz07@163.com>
2023-11-16 10:45:44 +0800

    run.sh小改动

--- xv6-x86-run_at_bochs/run.sh
+++ xv6-x86-run_at_bochs/run.sh
@@ -2,13 +2,13 @@
 
 #编译xv6 产生xv6.img、fs.img
 cd /crk/bochs/xv6-x86/ && \
-#make clean && \
+make clean && \
 make img && \
 rm -frv *.img.lock
 
 #编译bochs 产生可执行文件 /crk/bochs/bochs/bochs 
 cd /crk/bochs/bochs/ && \
 sh .conf.linux
-#make clean && \
+make all-clean && \
 make && \
 /crk/bochs/bochs/bochs -f /crk/bochs/xv6-x86-run_at_bochs/xv6-x86.bxrc 
