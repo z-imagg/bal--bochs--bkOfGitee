5cc265cf06e4801342c2b35620f4310816a3f0f4
prgrmz07 <prgrmz07@163.com>
2023-11-22 00:06:22 +0800

    解决：ibfmt报错： what():  invalid format string

--- xv6-x86-run_at_bochs/libfmt_demo/libfmt_demo.sh
+++ xv6-x86-run_at_bochs/libfmt_demo/libfmt_demo.sh
@@ -1,6 +1,4 @@
 g++ -I/pubx/fmt/include/ -g -O0 -ggdb -c libfmt_demo.cc
 g++ -o demo libfmt_demo.o /pubx/fmt/build/libfmt.a
 ./demo 
-# terminate called after throwing an instance of 'fmt::v10::format_error'
-#   what():  invalid format string
-# 已放弃 (核心已转储)
+# {"selector_value":"0x16", "selector_index":"0x157", "selector_ti":"0x2C", "selector_rpl":"0x37"}
