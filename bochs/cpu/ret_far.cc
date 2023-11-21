b0a3935c0a5a957ef6e601098c25afc301cc8351
prgrmz07 <prgrmz07@163.com>
2023-11-21 09:57:00 +0800

    精简后 逻辑清晰并加注释

--- bochs/cpu/ret_far.cc
+++ bochs/cpu/ret_far.cc
@@ -39,42 +39,39 @@ BX_CPU_C::return_protected(bxInstruction_c *i, Bit16u pop_bytes)
     if (BX_CPU_THIS_PTR sregs[BX_SEG_REG_SS].cache.u.segment.d_b) temp_RSP = ESP;
     else temp_RSP = SP;
 
-  if (i->os32L()) {
-    raw_cs_selector = (Bit16u) stack_read_dword(temp_RSP + 4);
-    return_RIP      =          stack_read_dword(temp_RSP);
-    stack_param_offset = 8;
+  if (i->os32L()) {//若是32位
+    raw_cs_selector = (Bit16u) stack_read_dword(temp_RSP + 4);//从栈中取 新代码段选择子CS
+    return_RIP      =          stack_read_dword(temp_RSP);//从栈中取  返回地址EIP(该代码段内的偏移量)
+    stack_param_offset = 8;//参数从栈偏移8开始
   }
-  else {
-    raw_cs_selector = stack_read_word(temp_RSP + 2);
-    return_RIP      = stack_read_word(temp_RSP);
-    stack_param_offset = 4;
+  else {//否则为64为
+    raw_cs_selector = stack_read_word(temp_RSP + 2);//从栈中取 新代码段选择子CS
+    return_RIP      = stack_read_word(temp_RSP);//从栈中取  返回地址RIP(该代码段内的偏移量)
+    stack_param_offset = 4;//参数从栈偏移4开始
   }
 
-  parse_selector(raw_cs_selector, &cs_selector);
+  parse_selector(raw_cs_selector, &cs_selector);//解析出 新代码段选择子CS cs_selector
 
-  // selector index must be within its descriptor table limits,
-  // else #GP(selector)
-  fetch_raw_descriptor(&cs_selector, &dword1, &dword2, BX_GP_EXCEPTION);
+  // 以 下标 取 元素 : 以 数组下标 获取 数组中该下标内的元素
+  fetch_raw_descriptor(&cs_selector, &dword1, &dword2, BX_GP_EXCEPTION);//以 下标 取 元素: 以 新代码段选择子CS cs_selector 取 新代码段描述符 
 
-  // descriptor AR byte must indicate code segment, else #GP(selector)
-  parse_descriptor(dword1, dword2, &cs_descriptor);
+  parse_descriptor(dword1, dword2, &cs_descriptor);//解析出 新代码段描述符cs_descriptor
 
   // check code-segment descriptor
   check_cs(&cs_descriptor, raw_cs_selector, 0, cs_selector.rpl);
 
-  // if return selector RPL == CPL then 若返回选择子的权限级RPL==当前代码段选择子的权限级CPL
-  // RETURN TO SAME PRIVILEGE LEVEL 则返回同PL权限级
-  if (cs_selector.rpl == CPL)
+  if (cs_selector.rpl == CPL)//若同PL权限级
   {
-    branch_far(&cs_selector, &cs_descriptor, return_RIP, CPL);//返回同PL权限级
+    branch_far(&cs_selector, &cs_descriptor, return_RIP, CPL);//跳转到 该新代码段(代码段描述符cs_descriptor) 内的 偏移量return_RIP 即可. 
+    //即 将 新代码段描述符cs_descriptor 装载入 代码段描述符寄存器, 并 跳转到 该新代码段内 偏移量return_RIP 处.
 
       if (BX_CPU_THIS_PTR sregs[BX_SEG_REG_SS].cache.u.segment.d_b)
-        RSP = ESP + stack_param_offset + pop_bytes;
+        RSP = ESP + stack_param_offset + pop_bytes;//新栈为当前栈跳过若干字节
       else
-         SP += stack_param_offset + pop_bytes;
+         SP += stack_param_offset + pop_bytes;//新栈为当前栈跳过若干字节
   }
   /* RETURN TO OUTER PRIVILEGE LEVEL 否则返回到不同的PL权限级, 从栈中 切换栈SS:SP 并 切换CS:IP */
-  else {
+  else {//否则,不同PL权限级
     /* + 6+N*2: SS      | +12+N*4:     SS | +24+N*8      SS */
     /* + 4+N*2: SP      | + 8+N*4:    ESP | +16+N*8     RSP */
     /*          parm N  | +        parm N | +        parm N */
@@ -85,31 +82,32 @@ BX_CPU_C::return_protected(bxInstruction_c *i, Bit16u pop_bytes)
     /* + 0:     IP      | + 0:        EIP | + 0:        RIP */
 
     if (i->os32L()) {
-      raw_ss_selector = stack_read_word(temp_RSP + 12 + pop_bytes);
-      return_RSP      = stack_read_dword(temp_RSP + 8 + pop_bytes);
+      raw_ss_selector = stack_read_word(temp_RSP + 12 + pop_bytes);//从 当前栈 中 取 新栈段选择子ss
+      return_RSP      = stack_read_dword(temp_RSP + 8 + pop_bytes);//从 当前栈 中 取 新ESP
     }
     else {
-      raw_ss_selector = stack_read_word(temp_RSP + 6 + pop_bytes);
-      return_RSP      = stack_read_word(temp_RSP + 4 + pop_bytes);
+      raw_ss_selector = stack_read_word(temp_RSP + 6 + pop_bytes);//从 当前栈 中 取 新栈段选择子ss
+      return_RSP      = stack_read_word(temp_RSP + 4 + pop_bytes);//从 当前栈 中 取 新RSP
     }
 
-    parse_selector(raw_ss_selector, &ss_selector);//解析选择子
+    parse_selector(raw_ss_selector, &ss_selector);//解析出 新栈段选择子ss ss_selector
 
-      fetch_raw_descriptor(&ss_selector, &dword1, &dword2, BX_GP_EXCEPTION);//选择子(数组下标) 转为 描述符(该数组内该下标中的元素)
-      parse_descriptor(dword1, dword2, &ss_descriptor);//解析描述符
+      fetch_raw_descriptor(&ss_selector, &dword1, &dword2, BX_GP_EXCEPTION);//以 下标 取 元素: 以 新栈段选择子ss ss_selector 取 新栈段描述符
+      parse_descriptor(dword1, dword2, &ss_descriptor);//解析出 新栈段描述符ss_descriptor
     
-    branch_far(&cs_selector, &cs_descriptor, return_RIP, cs_selector.rpl);//返回不同PL权限级
+    branch_far(&cs_selector, &cs_descriptor, return_RIP, cs_selector.rpl);//跳转到 该新代码段(新代码段描述符cs_descriptor) 内的 偏移量return_RIP. 
+    //即 将 新代码段描述符cs_descriptor 装载入 代码段描述符寄存器, 并 跳转到 该新代码段内 偏移量return_RIP 处.
 
     if ((raw_ss_selector & 0xfffc) != 0) {
       // load SS:RSP from stack
       // load the SS-cache with SS descriptor
-      load_ss(&ss_selector, &ss_descriptor, cs_selector.rpl);
+      load_ss(&ss_selector, &ss_descriptor, cs_selector.rpl);//将 新栈段描述符ss_descriptor 装载入 栈段描述符寄存器
     }
 
       if (ss_descriptor.u.segment.d_b)
-        RSP = (Bit32u)(return_RSP + pop_bytes);
+        RSP = (Bit32u)(return_RSP + pop_bytes);//新栈与当前栈无关
       else
-        SP  = (Bit16u)(return_RSP + pop_bytes);
+        SP  = (Bit16u)(return_RSP + pop_bytes);//新栈与当前栈无关
 
     /* check ES, DS, FS, GS for validity */
     validate_seg_regs();
