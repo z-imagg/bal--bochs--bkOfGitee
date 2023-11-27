f6060576a7131ecd251b5b15f13213db08223a81
prgrmz01@163.com <prgrmz01@163.com>
2023-11-27 16:33:25 +0800

    整体&&\  (空行可维持不需要&&\)

--- linux2.6-run_at_bochs/build-linux-2.6.27.15-on-i386_ubuntu14.04.6LTS.sh
+++ linux2.6-run_at_bochs/build-linux-2.6.27.15-on-i386_ubuntu14.04.6LTS.sh
@@ -66,9 +66,9 @@ dpkg -l | awk '/^ii/ {print $2}' | grep -E '^g\+\+-4\.[0-9]$|^gcc-4\.[0-9]$' | x
 #^ii 只要ii开头的; $2 只要第一列 即 包名列, 后面还有很多列都不要.
 echo " 开始: 安装gcc-4.4、g++-4.4" && \
 sudo apt install -y gcc-4.4 g++-4.4 && \
-{ \ 
- { [ -f /usr/bin/gcc ] && sudo mv /usr/bin/gcc /usr/bin/gcc.old ; } || \
- { [ -f /usr/bin/g++ ] && sudo mv /usr/bin/g++ /usr/bin/g++.old ; } || \
+{ \
+ { [ -f /usr/bin/gcc ] && sudo mv /usr/bin/gcc /usr/bin/gcc.old ; \
+   [ -f /usr/bin/g++ ] && sudo mv /usr/bin/g++ /usr/bin/g++.old ; } || \
  #这两行并非必须，若不存在 /usr/bin/gcc|g++ 这两行会执行失败，但后续命令应该继续. 因此下一行有个true,确保继续执行后续.
  true ;} && \
 gcc_4_4_bin=$(findCmdByDebPkgName gcc-4.4 gcc-4.4) && \
@@ -78,35 +78,37 @@ gpp_4_4_bin=$(findCmdByDebPkgName g++-4.4 g++-4.4) && \
 # sudo ln -s /usr/bin/gcc-4.4 /usr/bin/gcc && \
 # sudo ln -s /usr/bin/g++-4.4 /usr/bin/g++ && \
 echo " 完成: 安装gcc-4.4、g++-4.4" && \
-which gcc ; }
+which gcc ; \
+} && \
 
 
 
 #0. 编译环境
 
-cat /etc/issue
+cat /etc/issue && \
 #Ubuntu 14.04.6 LTS \n \l
 
-uname -a
+uname -a && \
 #Linux x 4.4.0-142-generic #168~14.04.1-Ubuntu SMP Sat Jan 19 11:28:33 UTC 2019 i686 i686 i686 GNU/Linux
 
 
-gcc --version
+gcc --version && \
 #gcc (Ubuntu/Linaro 4.4.7-8ubuntu1) 4.4.7
 
 
 #1. 下载内核源码包并验证签名
-kernelFUrl="https://mirrors.cloud.tencent.com/linux-kernel/v2.6/linux-2.6.27.15.tar.gz"
-kernelSumFUrl=https://mirrors.edge.kernel.org/pub/linux/kernel/v2.6/sha256sums.asc
-kernelF="linux-2.6.27.15.tar.gz"
-kernelF_="linux-2.6.27.15"
-kernelSumF="sha256sums.asc"
+kernelFUrl="https://mirrors.cloud.tencent.com/linux-kernel/v2.6/linux-2.6.27.15.tar.gz" && \
+kernelSumFUrl=https://mirrors.edge.kernel.org/pub/linux/kernel/v2.6/sha256sums.asc && \
+kernelF="linux-2.6.27.15.tar.gz" && \
+kernelF_="linux-2.6.27.15" && \
+kernelSumF="sha256sums.asc" && \
 
 { test -f $kernelF && test -f $kernelSumF && \
 grep  $kernelF  $kernelSumF | sha256sum --check  - &&  \
 echo "已经下载 : kernelFile=$kernelF,kernelSumF=$kernelSumF" ; } || \
 { wget $kernelFUrl --output-document  $kernelF && \
-wget $kernelSumFUrl --output-document $kernelSumF ; } 
+wget $kernelSumFUrl --output-document $kernelSumF ; \
+} && \ 
 
 
 
@@ -116,8 +118,8 @@ wget $kernelSumFUrl --output-document $kernelSumF ; }
 #2. linux2.6内核编译过程
 
 #2.1解压内核
-tar -zxvf $kernelF
-cd $kernelF_
+tar -zxvf $kernelF && \
+cd $kernelF_ && \
 
 #解决报错/Error1
 
@@ -127,20 +129,20 @@ echo '解决以下报错:
 gcc: error: elf_i386: No such file or directory
 gcc: error: unrecognized command line option ‘-m’
 原因: gcc 4.6不再支持linker-style架构（我使用的是gcc 4.8.4）。
-解决: 替换 "-m elf_i386" 为 "-m32": \'sed -i  "s/-m elf_i386/-m32/" arch/x86/vdso/Makefile\'
+解决: 替换 "-m elf_i386" 为 "-m32": \'sed -i  "s/-m elf_i386/-m32/" arch/x86/vdso/Makefile\'  && \
 
-sed -i  "s/-m elf_i386/-m32/" arch/x86/vdso/Makefile
-
-
+sed -i  "s/-m elf_i386/-m32/" arch/x86/vdso/Makefile && \
 
 
 
-make menuconfig
+
+
+make menuconfig && \
 # 人工选择: Exit ---> 保存。  
 # 即 不做任何变动
 
 #开始编译
-make -V=1
+make -V=1 && \
 
 #正常编译完成
 
@@ -148,7 +150,7 @@ make -V=1
 #3. 编译产物中的ELF
 
 #pwd==/crk/bochs/linux2.6-run_at_bochs/linux-2.6.27.15
-find . -not -name "*.o"  -and -not -name "*.h" -and -not -name "*.c" -and -not -name "*.ko" -and -not -name "*.so" -and -not -name "*.dbg"  -exec file {} \; | grep "ELF 32-bit LSB"
+{ find . -not -name "*.o"  -and -not -name "*.h" -and -not -name "*.c" -and -not -name "*.ko" -and -not -name "*.so" -and -not -name "*.dbg"  -exec file {} \; | grep "ELF 32-bit LSB" ; } && \
 
 
 
