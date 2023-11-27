200f2cd83bdaaf4336f7d06e2a633aa26bbbcd1d
prgrmz01@163.com <prgrmz01@163.com>
2023-11-27 20:17:20 +0800

    bochs正常启动linux2.6.27.15: 磁盘映像从10MB调大到50MB，问题消失
    c7eab5e69c7f8db59103269a844bbef43ddd67e7 即 问题：大文件(3MB)bzImage放到fat12分区中, bochs的bios或mbr界面无grub
    40C 16H 32S --> 200C 16H 32S， 即 磁盘映像从10MB调大到50MB，此问题消失， 即 bochs正常启动linux2.6.27.15

--- linux2.6-run_at_bochs/bochs2.7boot-grub4dos-linux2.6.27.15.sh
+++ linux2.6-run_at_bochs/bochs2.7boot-grub4dos-linux2.6.27.15.sh
@@ -1,33 +1,34 @@
 #0. 安装mkdiskimage命令
 # { apt-file --help 2>/dev/null 1>/dev/null && echo "已安装apt-file(搜索命令对应的.deb安装包)" && apt-file search mkdiskimage ; } || { sudo apt install -y apt-file && sudo apt-file update && echo "apt-file(搜索命令对应的.deb安装包)安装完毕" ; }
-{ mkdiskimage 2>/dev/null 1>/dev/null && echo "已经安装mkdiskimage" ; } || { sudo apt install -y syslinux syslinux-common syslinux-efi syslinux-utils ; echo "mkdiskimage安装完毕(mkdiskimage由syslinux-util提供, 但是syslinux syslinux-common syslinux-efi都要安装,否则mkdiskimage产生的此 HD10MB40C16H32S.img 几何参数不对、且 分区没格式化 )"; }
+{ mkdiskimage 2>/dev/null 1>/dev/null && echo "已经安装mkdiskimage" ; } || { sudo apt install -y syslinux syslinux-common syslinux-efi syslinux-utils ; echo "mkdiskimage安装完毕(mkdiskimage由syslinux-util提供, 但是syslinux syslinux-common syslinux-efi都要安装,否则mkdiskimage产生的此 HD50MB200C16H32S.img 几何参数不对、且 分区没格式化 )"; }
 
 #1. 制作硬盘镜像、注意磁盘几何参数得符合bochs要求、仅1个fat12分区
-sudo umount /mnt/hd_img 2>/dev/null ; sudo rm -frv /mnt/hd_img ; rm -fv HD10MB40C16H32S.img
-PartitionFirstByteOffset=$(mkdiskimage -o   HD10MB40C16H32S.img 40 16 32)
-#  有可能 此命令 并没有正确设置磁盘映像文件10MB.img的几何参数为 40C 16H 32S
+sudo umount /mnt/hd_img 2>/dev/null ; sudo rm -frv /mnt/hd_img ; rm -fv HD50MB200C16H32S.img
+PartitionFirstByteOffset=$(mkdiskimage -o   HD50MB200C16H32S.img 200 16 32)
+#  有可能 此命令 并没有正确设置磁盘映像文件10MB.img的几何参数为 200C 16H 32S
 # PartitionFirstByteOffset==$((32*512))==16384
 
 
-#xxd -seek +0X1C3 -len 3 HD10MB40C16H32S.img
+#xxd -seek +0X1C3 -len 3 HD50MB200C16H32S.img
 #0X1C3:0X0F:15:即16H:即16个磁头, 0X1C4:0X20:32:即32S:即每磁道有32个扇区, 0X1C3:0X27:39:即40C:即40个柱面
 
-{ test "$(xxd -seek +0X1C3 -len 3  -plain  HD10MB40C16H32S.img)" == "0f2027" && echo "磁盘几何参数指定成功" ; }  || { echo "磁盘几何参数指定失败, 为确认 请用diskgenius专业版打开该x.img查看几何参数" && exit 5; }
+#todo 这个0f2027要重新用dg查看
+# { test "$(xxd -seek +0X1C3 -len 3  -plain  HD50MB200C16H32S.img)" == "0f2027" && echo "磁盘几何参数指定成功" ; }  || { echo "磁盘几何参数指定失败, 为确认 请用diskgenius专业版打开该x.img查看几何参数" && exit 5; }
 
-echo "注意sfdisk显示磁盘的几何参数与diskgenius的不一致,这里认为sfdisk是错误的，而diskgenius是正确的" && sfdisk --show-geometry HD10MB40C16H32S.img
+echo "注意sfdisk显示磁盘的几何参数与diskgenius的不一致,这里认为sfdisk是错误的，而diskgenius是正确的" && sfdisk --show-geometry HD50MB200C16H32S.img
 
-# parted -s  HD10MB40C16H32S.img mklabel msdos
-# parted -s  HD10MB40C16H32S.img mkpart primary fat16 2048s 100%
-# parted -s  HD10MB40C16H32S.img set 1 boot on
+# parted -s  HD50MB200C16H32S.img mklabel msdos
+# parted -s  HD50MB200C16H32S.img mkpart primary fat16 2048s 100%
+# parted -s  HD50MB200C16H32S.img set 1 boot on
 
-# mkfs.vfat -F 16 -n C HD10MB40C16H32S.img
+# mkfs.vfat -F 16 -n C HD50MB200C16H32S.img
 
 
 
 
 
 #2. 用grubinst.exe安装grldr.mbr到磁盘镜像
-echo "执行grubinst.exe前md5sum: $(md5sum HD10MB40C16H32S.img)"
+echo "执行grubinst.exe前md5sum: $(md5sum HD50MB200C16H32S.img)"
 
 
 #借助win10中的grubinst_1.0.1_bin_win安装grldr.mbr
@@ -42,20 +43,20 @@ win10SshPassF=/win10SshPass
 #若无sshpass则安装
 { sshpass -V 2>/dev/null 1>/dev/null && echo "已经安装sshpass" ; } || { sudo apt install -y sshpass ; echo "sshpass安装完毕"; }
 
-sshpass -p $win10SshPass scp  -P $win10SshPort HD10MB40C16H32S.img zzz@$win10Host:/HD10MB40C16H32S.img 
+sshpass -p $win10SshPass scp  -P $win10SshPort HD50MB200C16H32S.img zzz@$win10Host:/HD50MB200C16H32S.img 
 
 sshpass -p $win10SshPass ssh -p $win10SshPort zzz@$win10Host "test -f  /grubinst_1.0.1_bin_win/grubinst/grubinst.exe || { wget https://sourceforge.net/projects/grub4dos/files/grubinst/grubinst%201.0.1/grubinst_1.0.1_bin_win.zip/download  --output-document   /grubinst_1.0.1_bin_win.zip && pacman --noconfirm -S  unzip && unzip -o /grubinst_1.0.1_bin_win.zip -d / ; }"
 
-sshpass -p $win10SshPass ssh -p $win10SshPort zzz@$win10Host "/grubinst_1.0.1_bin_win/grubinst/grubinst.exe /HD10MB40C16H32S.img && echo 'grubinst.exe ok'"
+sshpass -p $win10SshPass ssh -p $win10SshPort zzz@$win10Host "/grubinst_1.0.1_bin_win/grubinst/grubinst.exe /HD50MB200C16H32S.img && echo 'grubinst.exe ok'"
 
-sshpass -p $win10SshPass scp   -P $win10SshPort  zzz@$win10Host:/HD10MB40C16H32S.img  HD10MB40C16H32S.img
+sshpass -p $win10SshPass scp   -P $win10SshPort  zzz@$win10Host:/HD50MB200C16H32S.img  HD50MB200C16H32S.img
 #注: $win10Host:/ == D:\msys64, 所以请实现复制 grubinst_1.0.1_bin_win 到 D:\msys64\下
 
-echo "执行grubinst.exe后md5sum: $(md5sum HD10MB40C16H32S.img)"
+echo "执行grubinst.exe后md5sum: $(md5sum HD50MB200C16H32S.img)"
 
 sudo mkdir /mnt/hd_img
-sudo mount -o loop,offset=$PartitionFirstByteOffset HD10MB40C16H32S.img /mnt/hd_img
-# sudo losetup --offset $((32*512)) /dev/loop15 HD10MB40C16H32S.img
+sudo mount -o loop,offset=$PartitionFirstByteOffset HD50MB200C16H32S.img /mnt/hd_img
+# sudo losetup --offset $((32*512)) /dev/loop15 HD50MB200C16H32S.img
 # sudo mount -o loop /dev/loop15 /mnt/hd_img
 test -f grub4dos-0.4.4.zip || { echo "下载grub4dos-0.4.4.zip" && wget https://jaist.dl.sourceforge.net/project/grub4dos/GRUB4DOS/grub4dos%200.4.4/grub4dos-0.4.4.zip ; }
 md5sum --check  md5sum.grub4dos-0.4.4.zip.txt || { echo "grub4dos-0.4.4.zip的md5sum错,退出码为6" && exit 6; }
@@ -87,7 +88,7 @@ sudo cp -v grub4dos-0.4.4/grldr  menu.lst  /mnt/hd_img/
 #问题现象:  
 # 0. 若复制3MB的bzImage，则bochs的bios或mbr启动界面没进grub.  反之, bochs启动界面bios能进grub.
 # 1. diskgenious下打开.img 内无文件. (提交 de98c29a7bc2e284473c222b1c9a7e4ec82872ec 也有此问题，但bochs正常进入grub菜单)
-# { test -f $bzImageF  && echo $okMsg1 && sudo cp -v $bzImageF  /mnt/hd_img/; } || { echo $errMsg2  && exit 8 ;  } 
+{ test -f $bzImageF  && echo $okMsg1 && sudo cp -v $bzImageF  /mnt/hd_img/; } || { echo $errMsg2  && exit 8 ;  } 
 
 #卸载磁盘映像文件
 read -p "即将卸载"
@@ -108,7 +109,7 @@ vgaromimage: file=/crk/bochs/bochs/bios/VGABIOS-lgpl-latest
 #romimage: file=D:\Bochs-2.6.11\BIOS-bochs-latest
 #vgaromimage: file=D:\Bochs-2.6.11\VGABIOS-lgpl-latest
 
-ata0-master: type=disk, path="HD10MB40C16H32S.img", cylinders=40, heads=16, spt=32
+ata0-master: type=disk, path="HD50MB200C16H32S.img", cylinders=40, heads=16, spt=32
 boot: c
 log: bochsout.txt
 mouse: enabled=0
