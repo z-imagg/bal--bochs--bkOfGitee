5eb2b4efe4148c6c46398421b66399558fee865f
prgrmz01@163.com <prgrmz01@163.com>
2023-11-27 11:05:41 +0800

    ubuntu14.04下git版本升级脚本(判断版本、隐式if-else、&& \连接可以放注释行)

--- linux2.6-run_at_bochs/build-linux-2.6.27.15-on-i386_ubuntu14.04.6LTS.sh
+++ linux2.6-run_at_bochs/build-linux-2.6.27.15-on-i386_ubuntu14.04.6LTS.sh
@@ -1,5 +1,25 @@
 #正常编译 linux-2.6.27.15.tar.gz 在 "真机 i386/ubuntu14.04.6LTS"
 
+#升级git到2.x版本
+#  ubuntu14.04 自带git版本为1.9, lazygit目前主流版本最低支持git2.0, 因此要升级git版本
+
+{ curGitVer=`git --version` && \
+[[ "$curGitVer" > "git version 2._" ]] && \
+#ascii码表中 '0'>'_' 从而决定了 :  "git version 2.0" > "git version 2._"
+echo "git版本无需升级,已为2.x:$curGitVer" ; } || \
+{ echo "git版本($curGitVer)过低,现在升级git" && \
+sudo add-apt-repository --yes ppa:git-core/ppa && \
+sudo apt-get update 1>/dev/null && \
+ { sudo apt-cache show  git | grep Version ; } && \
+#Version: 1:2.29.0-0ppa1~ubuntu14.04.1
+sudo apt-get install -y git && \
+git --version && \
+#git version 2.29.0
+sudo add-apt-repository --yes --remove ppa:git-core/ppa && \
+curGitVer=`git --version` && \
+echo "git版本升级完成,已升级到版本($curGitVer)" ; }
+
+
 
 #报错解决/Error2
 
