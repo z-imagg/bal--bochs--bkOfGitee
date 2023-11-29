abe49f90a655a3466ff0c33ab608ef2e457cca58
prgrmz01@163.com <prgrmz01@163.com>
2023-11-29 17:37:29 +0800

    修

--- linux2.6-run_at_bochs/install_grubinst_on_win10_by_msys2.sh
+++ linux2.6-run_at_bochs/install_grubinst_on_win10_by_msys2.sh
@@ -4,12 +4,16 @@
 #用bash执行此脚本
 #请在 mingw(msys2) 的 /etc/profile 末尾加上  export ubt22x64Pass=ubuntu22x64用户z的登录密码  并 不要上传该密码到git仓库
 
+#请在win10主机操作系统的本地域名解析文件 c:\windows\system32\drivers\etc\hosts 中添加 :
+# 192.168.1.4 ubt22x64Host
+# 否则此脚本失败
+
 set -e
 
 pacman --noconfirm -S   sshpass
 
 # 4.3 磁盘映像文件 复制到 ubt22x64主机msys2的根目录下
-sshpass -p $ubt22x64Pass scp  -P $ubt22x64Port  $ubt22x64User@$ubt22x64Host:/crk/bochs/linux2.6-run_at_bochs/$HdImgF /$HdImgF && \
+sshpass -p $ubt22x64Pass scp  -P $ubt22x64Port  $ubt22x64User@ubt22x64Host:/crk/bochs/linux2.6-run_at_bochs/$HdImgF /$HdImgF && \
 
 echo "执行grubinst.exe前md5sum: $(md5sum $HdImgF)" && \
 
@@ -28,7 +32,7 @@ unzip -o /grubinst_1.0.1_bin_win.zip -d / \
 # 4.5 ubt22x64主机上msys2:  用 grubinst.exe 对 磁盘映像文件 安装 grldr.mbr
 /grubinst_1.0.1_bin_win/grubinst/grubinst.exe /$HdImgF && echo 'grubinst.exe ok'
 #4.6 传回已 安装 grldr.mbr 的 磁盘映像文件
-sshpass -p $ubt22x64Pass scp  -P $ubt22x64Port  /$HdImgF  $ubt22x64User@$ubt22x64Host:/crk/bochs/linux2.6-run_at_bochs/$HdImgF  && \
+sshpass -p $ubt22x64Pass scp  -P $ubt22x64Port  /$HdImgF  $ubt22x64User@ubt22x64Host:/crk/bochs/linux2.6-run_at_bochs/$HdImgF  && \
 #注: $win10Host:/ == D:\msys64, 所以请事先复制 grubinst_1.0.1_bin_win 到 D:\msys64\下
 
 echo "执行grubinst.exe后md5sum: $(md5sum $HdImgF)"
