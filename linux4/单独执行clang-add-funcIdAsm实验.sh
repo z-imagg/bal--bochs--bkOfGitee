9aed8a9f880e8c270fb3cfc69a8ee89f7bbbfab7
prgrmz01@163.com <prgrmz01@163.com>
2023-12-05 20:12:02 +0800

    又补充了-D、且解决了报错BUILD_BUG_ON（增加选项-D__OPTIMIZE__），报错又少了

--- linux4/单独执行clang-add-funcIdAsm实验.sh
+++ linux4/单独执行clang-add-funcIdAsm实验.sh
@@ -7,14 +7,59 @@ cd /home/z/linux-4.14.259
 #   i686-linux-gnu-gcc -Wp,-MD,net/netfilter/.xt_addrtype.o.d -nostdinc -isystem /usr/lib/gcc-cross/i686-linux-gnu/11/include -I./arch/x86/include -I./arch/x86/include/generated  -I./include -I./arch/x86/include/uapi -I./arch/x86/include/generated/uapi -I./include/uapi -I./include/generated/uapi -include ./include/linux/kconfig.h -D__KERNEL__ -Wall -Wundef -Wstrict-prototypes -Wno-trigraphs -fno-strict-aliasing -fno-common -fshort-wchar -Werror-implicit-function-declaration -Wno-format-security -std=gnu89 -fno-PIE -mno-sse -mno-mmx -mno-sse2 -mno-3dnow -mno-avx -fcf-protection=none -m32 -msoft-float -mregparm=3 -freg-struct-return -fno-pic -mpreferred-stack-boundary=2 -march=i686 -mtune=generic -Wa,-mtune=generic32 -ffreestanding -DCONFIG_AS_CFI=1 -DCONFIG_AS_CFI_SIGNAL_FRAME=1 -DCONFIG_AS_CFI_SECTIONS=1 -DCONFIG_AS_SSSE3=1 -DCONFIG_AS_CRC32=1 -DCONFIG_AS_AVX=1 -DCONFIG_AS_AVX2=1 -DCONFIG_AS_AVX512=1 -DCONFIG_AS_SHA1_NI=1 -DCONFIG_AS_SHA256_NI=1 -pipe -Wno-sign-compare -fno-asynchronous-unwind-tables -mindirect-branch=thunk-extern -mindirect-branch-register -fno-jump-tables -fno-delete-null-pointer-checks -Wno-frame-address -Wno-format-truncation -Wno-format-overflow -Wno-int-in-bool-context -Wno-address-of-packed-member -Wno-attribute-alias -O2 -fno-allow-store-data-races -DCC_HAVE_ASM_GOTO -Wframe-larger-than=1024 -fno-stack-protector -Wno-unused-but-set-variable -Wno-unused-const-variable -fno-omit-frame-pointer -fno-optimize-sibling-calls -fno-var-tracking-assignments -Wdeclaration-after-statement -Wno-pointer-sign -Wno-stringop-truncation -Wno-zero-length-bounds -Wno-array-bounds -Wno-stringop-overflow -Wno-restrict -Wno-maybe-uninitialized -fno-strict-overflow -fno-merge-all-constants -fmerge-constants -fno-stack-check -fconserve-stack -Werror=implicit-int -Werror=strict-prototypes -Werror=date-time -Werror=incompatible-pointer-types -Werror=designated-init -fmacro-prefix-map=./= -Wno-packed-not-aligned  -DMODULE  -DKBUILD_BASENAME='"xt_addrtype"'  -DKBUILD_MODNAME='"xt_addrtype"' -c -o net/netfilter/xt_addrtype.o net/netfilter/xt_addrtype.c
 
 
+# -Wa,-mtune=generic32 \
+# -Wno-shift-overflow  -Wno-zero-length-array -Wno-shift-overflow  -Wno-uninitialized -Wno-over-aligned -Wunknown-warning-option  # clang选项和gcc选项有差异，这些是clang选项， 根据clang报错 提示 的来的 
+
+#
 #直接改造为:
-/app/llvm_release_home/clang+llvm-15.0.0-x86_64-linux-gnu-rhel-8.4/bin/clang  -std=gnu89   -D__KERNEL__  -Wall -Wundef  -DCONFIG_AS_CFI=1 -DCONFIG_AS_CFI_SIGNAL_FRAME=1 -DCONFIG_AS_CFI_SECTIONS=1 -DCONFIG_AS_SSSE3=1 -DCONFIG_AS_CRC32=1 -DCONFIG_AS_AVX=1 -DCONFIG_AS_AVX2=1 -DCONFIG_AS_AVX512=1 -DCONFIG_AS_SHA1_NI=1 -DCONFIG_AS_SHA256_NI=1   -DCC_HAVE_ASM_GOTO   -DMODULE  -DKBUILD_BASENAME='"xt_addrtype"'  -DKBUILD_MODNAME='"xt_addrtype"'  -isystem /usr/lib/gcc-cross/i686-linux-gnu/11/include -I./arch/x86/include -I./arch/x86/include/generated  -I./include -I./arch/x86/include/uapi -I./arch/x86/include/generated/uapi -I./include/uapi -I./include/generated/uapi -include ./include/linux/kconfig.h  -Xclang   -load -Xclang  /crk/clang-add-funcIdAsm/build/lib/libCTk.so  -Xclang   -add-plugin -Xclang  CTk      -c  net/netfilter/xt_addrtype.c
+/app/llvm_release_home/clang+llvm-15.0.0-x86_64-linux-gnu-rhel-8.4/bin/clang \
+-D__OPTIMIZE__ \
+-Wp,-MD,net/netfilter/.xt_addrtype.o.d \
+-Wall -Wundef -Wstrict-prototypes -Wno-trigraphs \
+-Werror-implicit-function-declaration -Wno-format-security \
+-Wno-sign-compare \
+-Wno-frame-address -Wno-format-truncation -Wno-format-overflow -Wno-int-in-bool-context -Wno-address-of-packed-member -Wno-attribute-alias \
+-Wframe-larger-than=1024 \
+-Wno-unused-but-set-variable -Wno-unused-const-variable -fno-omit-frame-pointer \
+-Wdeclaration-after-statement -Wno-pointer-sign -Wno-stringop-truncation -Wno-zero-length-bounds -Wno-array-bounds -Wno-stringop-overflow -Wno-restrict -Wno-maybe-uninitialized \
+-Werror=implicit-int -Werror=strict-prototypes -Werror=date-time -Werror=incompatible-pointer-types -Werror=designated-init \
+-Wno-packed-not-aligned \
+ -std=gnu89   -D__KERNEL__    -DCONFIG_AS_CFI=1 -DCONFIG_AS_CFI_SIGNAL_FRAME=1 -DCONFIG_AS_CFI_SECTIONS=1 -DCONFIG_AS_SSSE3=1 -DCONFIG_AS_CRC32=1 -DCONFIG_AS_AVX=1 -DCONFIG_AS_AVX2=1 -DCONFIG_AS_AVX512=1 -DCONFIG_AS_SHA1_NI=1 -DCONFIG_AS_SHA256_NI=1   -DCC_HAVE_ASM_GOTO   -DMODULE  -DKBUILD_BASENAME='"xt_addrtype"'  -DKBUILD_MODNAME='"xt_addrtype"'  -isystem /usr/lib/gcc-cross/i686-linux-gnu/11/include -I./arch/x86/include -I./arch/x86/include/generated  -I./include -I./arch/x86/include/uapi -I./arch/x86/include/generated/uapi -I./include/uapi -I./include/generated/uapi -include ./include/linux/kconfig.h  -Xclang   -load -Xclang  /crk/clang-add-funcIdAsm/build/lib/libCTk.so  -Xclang   -add-plugin -Xclang  CTk      -c  net/netfilter/xt_addrtype.c
 
 #   （由于 clang插件只是语法层面的，因此 上述 命令中  后端部分参数    -m32   -march=i686     可以丢弃）
 
 #会报错:
+
+
+# ./arch/x86/include/asm/ptrace.h:208:16: error: no member named 'ds' in 'pt_regs'
+            # offset == offsetof(struct pt_regs, ds) ||
+                      # ^                        ~~
+# ./include/linux/stddef.h:17:32: note: expanded from macro 'offsetof'
+# define offsetof(TYPE, MEMBER)  __compiler_offsetof(TYPE, MEMBER)
+
+
+# ./arch/x86/include/asm/ptrace.h:209:16: error: no member named 'es' in 'pt_regs'
+            # offset == offsetof(struct pt_regs, es) ||
+                      # ^                        ~~
+# ./include/linux/stddef.h:17:32: note: expanded from macro 'offsetof'
+# define offsetof(TYPE, MEMBER)  __compiler_offsetof(TYPE, MEMBER)
+
+
+# ./arch/x86/include/asm/ptrace.h:210:16: error: no member named 'fs' in 'pt_regs'
+            # offset == offsetof(struct pt_regs, fs) ||
+
+#修复报错 BUILD_BUG_ON :
+#  根据 linux-4.14.259/include/linux/build_bug.h : 67 行: 可知 ，打开宏 __OPTIMIZE__ (即增加选项-D__OPTIMIZE__）  即可 关闭 BUILD_BUG_ON，
+#ifndef __OPTIMIZE__
+#define BUILD_BUG_ON(condition) ((void)sizeof(char[1 - 2*!!(condition)]))
+#else
+#define BUILD_BUG_ON(condition) \
+	BUILD_BUG_ON_MSG(condition, "BUILD_BUG_ON failed: " #condition)
+#endif
+
 # ./include/net/dst.h:255:2: error: array size is negative
         # BUILD_BUG_ON(offsetof(struct dst_entry, __refcnt) & 63);
+ 
 # ./include/net/dst.h:255:2: error: array size is negative
         # BUILD_BUG_ON(offsetof(struct dst_entry, __refcnt) & 63);
 
