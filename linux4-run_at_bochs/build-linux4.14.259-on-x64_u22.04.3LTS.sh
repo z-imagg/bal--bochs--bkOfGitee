d3537d991886b8abbd9f45821d620f904f4673ca
prgrmz01@163.com <prgrmz01@163.com>
2023-12-06 16:34:06 +0800

    dev

--- linux4-run_at_bochs/build-linux4.14.259-on-x64_u22.04.3LTS.sh
+++ linux4-run_at_bochs/build-linux4.14.259-on-x64_u22.04.3LTS.sh
@@ -9,7 +9,7 @@ tar -zxf linux-4.14.259.tar.gz
 cd linux-4.14.259
 
 #
-sudo apt-get install gcc-multilib g++-multilib
+sudo apt-get install -y gcc-multilib g++-multilib
 make ARCH=i386 CROSS_COMPILE=i686-linux-gnu- defconfig
 make ARCH=i386 CROSS_COMPILE=i686-linux-gnu- menuconfig
 make ARCH=i386 CROSS_COMPILE=i686-linux-gnu- V=1 | tee -a make.log
