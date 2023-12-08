75990df2d2c2c0b22890189b3c1c592448f4f35b
prgrmz01@163.com <prgrmz01@163.com>
2023-12-08 16:11:38 +0800

    dev

--- linux4-run_at_bochs/build-linux4.14.259-on-x64_u22.04.3LTS.sh
+++ linux4-run_at_bochs/build-linux4.14.259-on-x64_u22.04.3LTS.sh
@@ -5,47 +5,50 @@
 git pull --no-edit && \
 git reset HEAD -- cmd-wrap && \
 git -C cmd-wrap stash --include-untracked && \
-git submodule update --init --force -- cmd-wrap )
+git submodule update --init --force -- cmd-wrap ) && \
 
 
 #cmd-wrap 拦截 gcc 命令
-[   -e /crk/cmd-wrap ] || ln -s /crk/bochs/cmd-wrap /crk/cmd-wrap
+{ [   -e /crk/cmd-wrap ] || ln -s /crk/bochs/cmd-wrap /crk/cmd-wrap ;} && \
 #install-wrap.sh内 会 将假gcc命令所在目录/crk/bin 放到 PATH最前面, 因此需要source执行。
-source /crk/cmd-wrap/install-wrap.sh
+source /crk/cmd-wrap/install-wrap.sh && \
 
 #ubuntu 22 x64
-sudo apt install -y gcc-11-i686-linux-gnu gcc-i686-linux-gnu
-sudo apt install -y gcc-multilib-i686-linux-gnu
+sudo apt install -y gcc-11-i686-linux-gnu gcc-i686-linux-gnu && \
+sudo apt install -y gcc-multilib-i686-linux-gnu && \
 # sudo apt-get install -y gcc-multilib g++-multilib
 
-LINUX=linux-4.14.259
-LINUX_tar_gz="${LINUX}.tar.gz"
-LINUX_tar_gz_md5sum_F="${LINUX_tar_gz}.md5sum.txt"
+LINUX=linux-4.14.259 && \
+LINUX_tar_gz="${LINUX}.tar.gz" && \
+LINUX_tar_gz_md5sum_F="${LINUX_tar_gz}.md5sum.txt" && \
 
-cd /crk/bochs/linux4-run_at_bochs/
+cd /crk/bochs/linux4-run_at_bochs/ && \
 
+{ \
 { [ -f $LINUX_tar_gz_md5sum_F ] && md5sum --check $LINUX_tar_gz_md5sum_F ;} || { rm -fr $LINUX_tar_gz  $LINUX && \
 wget https://mirrors.cloud.tencent.com/linux-kernel/v4.x/linux-4.14.259.tar.gz && \
-md5sum $LINUX_tar_gz > $LINUX_tar_gz_md5sum_F ;}
-tar -zxf $LINUX_tar_gz
-cd $LINUX
+md5sum $LINUX_tar_gz > $LINUX_tar_gz_md5sum_F ;} \
+} && \
+tar -zxf $LINUX_tar_gz && \
+cd $LINUX && \
 
 #并行编译 job数 为 max(核心数-1,1)
-job_n=$((nproc-1))
-job_n=$(( core_n > 1 ? core_n: 1 ))
+job_n=$((nproc-1)) && \
+job_n=$(( core_n > 1 ? core_n: 1 )) && \
 
-set -x
-make ARCH=i386 CROSS_COMPILE=i686-linux-gnu- defconfig
-make ARCH=i386 CROSS_COMPILE=i686-linux-gnu- menuconfig
-make ARCH=i386 CROSS_COMPILE=i686-linux-gnu- -j $job_n V=1 | tee -a make.log
-set +x
+set -x && \
+make ARCH=i386 CROSS_COMPILE=i686-linux-gnu- defconfig && \
+make ARCH=i386 CROSS_COMPILE=i686-linux-gnu- menuconfig && \
+{ make ARCH=i386 CROSS_COMPILE=i686-linux-gnu- -j $job_n V=1 | tee -a make.log ;} && \
+set +x && \
 
-find . -name "*bzImage*"
+find . -name "*bzImage*" && \
 # ./arch/x86/boot/bzImage
 # ./arch/x86/boot/.bzImage.cmd
 # ./arch/i386/boot/bzImage
-ls -lh arch/x86/boot/bzImage
+ls -lh arch/x86/boot/bzImage && \
 # -rw-rw-r-- 1   6.8M  arch/x86/boot/bzImage
- file arch/x86/boot/bzImage
+file arch/x86/boot/bzImage && \
 # arch/x86/boot/bzImage: Linux kernel x86 boot executable bzImage, version 4.14.259 (z@x) #1 SMP Tue Dec 5 19:10:09 CST 2023, RO-rootFS, swap_dev 0X6, Normal VGA
 
+_end=true
