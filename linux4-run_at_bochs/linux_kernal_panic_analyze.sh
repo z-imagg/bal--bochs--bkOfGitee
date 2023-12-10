4d2a4ff770833513e98e2da543a96be888bbb8db
prgrmz01@163.com <prgrmz01@163.com>
2023-12-10 12:26:44 +0800

    zz

--- linux4-run_at_bochs/linux_kernal_panic_analyze.sh
+++ linux4-run_at_bochs/linux_kernal_panic_analyze.sh
@@ -34,44 +34,52 @@ git rev-parse HEAD
 
 #牵涉的各函数 所在源文件 如下:
 grep mount_block_root funcIdDescLs.txt.csv
-# init/do_mounts.c,382,1,40013,mount_block_root,4,13
-grep mount_root funcIdDescLs.txt.csv
-# init/do_mounts.c,512,1,40015,mount_root,4,15
-grep prepare_namespace funcIdDescLs.txt.csv
-#init/do_mounts.c,549,1,40016,prepare_namespace,4,16
-grep kernel_init_freeable funcIdDescLs.txt.csv
-# init/main.c,1040,1,30029,kernel_init_freeable,3,29
-grep kernel_init funcIdDescLs.txt.csv
-# init/main.c,994,1,30000,kernel_init,3,0
-grep ret_from_fork funcIdDescLs.txt.csv
-#无函数 ret_from_fork
+    # init/do_mounts.c,382,1,40013,mount_block_root,4,13
+    grep mount_root funcIdDescLs.txt.csv
+    # init/do_mounts.c,512,1,40015,mount_root,4,15
+    grep prepare_namespace funcIdDescLs.txt.csv
+    #init/do_mounts.c,549,1,40016,prepare_namespace,4,16
+    grep kernel_init_freeable funcIdDescLs.txt.csv
+    # init/main.c,1040,1,30029,kernel_init_freeable,3,29
+    grep kernel_init funcIdDescLs.txt.csv
+    # init/main.c,994,1,30000,kernel_init,3,0
+    grep ret_from_fork funcIdDescLs.txt.csv
+    #无函数 ret_from_fork
 
 
 
 objdump --source     init/do_mounts.o > init/do_mounts.o.my.S
 "
-
 000001f4 <mount_block_root>:                                             #0. 此即  mount_block_root == 0X1f4
  1f4:	55                   	push   %ebp
  ...
- 32b:	e8 fc ff ff ff       	call   32c <mount_block_root+0x138>      #2.  仔细看此行  mount_block_root+0x138 == 0X32c, 跳转 的 目的地址 0X32C 并不是某指令的开头 此即 跳转到非法指令，因此崩溃。  (备注 地址0X32C 附近 只有 地址 0x32b 、 地址0x330 是指令开头）
+ 32b:	e8 fc ff ff ff       	call   32c <mount_block_root+0x138>      #2.  仔细看此行  mount_block_root+0x138 == 0X32c, 跳转 的 目的地址 0X32C 并不是某指令的开头 此即 跳转到非法指令，因此崩溃。  
+ (备注 地址0X32C 附近 只有 地址 0x32b 、 地址0x330 是指令开头）
  330:	f6 45 cc 01          	testb  $0x1,-0x34(%ebp)                  #1.  panic截图CallTrace中的 'mount_block_root+0x13c' == 0X330 即此行, 因此崩溃的是上一行
  334:	75 09                	jne    33f <mount_block_root+0x14b>
  336:	83 4d cc 01          	orl    $0x1,-0x34(%ebp)
  33a:	e9 4e ff ff ff       	jmp    28d <mount_block_root+0x99>
-"
-
-
-
-"
 
 000003a5 <mount_root>:                                       #0. 此即  mount_root == 0X3a5
  3a5:	55                   	push   %ebp
  ...
- 491:	e8 fc ff ff ff       	call   492 <mount_root+0xed> #2. mount_root+0xed == 0x492, 跳转 的 目的地址 0x492 并不是某指令的开头 此即 跳转到非法指令，因此崩溃。  (备注 地址 0x492 附近 只有 地址 0x491 、 地址0x496 是指令开头）
+ 491:	e8 fc ff ff ff       	call   492 <mount_root+0xed> #2. mount_root+0xed == 0x492, 跳转 的 目的地址 0x492 并不是某指令的开头 此即 跳转到非法指令，因此崩溃。  
+ (备注 地址 0x492 附近 只有 地址 0x491 、 地址0x496 是指令开头）
  496:	8d 65 f8             	lea    -0x8(%ebp),%esp       #1. panic截图CallTrace中的 'mount_root+0xf1' == 0x496 , 即此行，因此奔溃的是上一行.
  499:	5b                   	pop    %ebx
  49a:	5e                   	pop    %esi
  49b:	5d                   	pop    %ebp
  49c:	c3                   	ret    
+"
+
+objdump --source init/main.o > init/main.o.my.S
+"
+0000049d <prepare_namespace>:                                            #0. 此即  prepare_namespace == 0X49d
+ 49d:	55                   	push   %ebp
+ ...
+ 5b9:	e8 fc ff ff ff       	call   5ba <prepare_namespace+0x11d>     #2. prepare_namespace+0x11d == 0x5ba, 跳转 的 目的地址 0x5ba 并不是某指令的开头 此即 跳转到非法指令，因此崩溃。  
+ (备注 地址 0x5ba 附近 只有 地址 0x5b9 、 地址 0x5be 是指令开头）
+ 5be:	b8 83 00 00 00       	mov    $0x83,%eax                        #1. prepare_namespace+0x121 == 0x5be
+ ...
+ 5ee:	c3       
 "
