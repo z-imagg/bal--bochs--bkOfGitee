9f00d45112b150f9275a056541481b9baf351abe
prgrmz07 <prgrmz07@163.com>
2024-01-06 18:54:25 +0800

    logXv6X86FuncId更名为logFnEnt

--- bochs/cpu/ctrl_xfer32.cc
+++ bochs/cpu/ctrl_xfer32.cc
@@ -277,7 +277,7 @@ void BX_CPP_AttrRegparmN(1) BX_CPU_C::CALL32_Ep(bxInstruction_c *i)
   BX_NEXT_TRACE(i);
 }
 
-void BX_CPU_C::logXv6X86FuncId(bxInstruction_c *instr){
+void BX_CPU_C::logFnEnt(bxInstruction_c *instr){
 
   //取得 当前指令 紧挨着的 4个字节
   Bit32u or1_instr_appendByte=BX_CPU_THIS->read_linear_dword (instr->seg(),EIP);
@@ -336,7 +336,7 @@ void BX_CPU_C::logXv6X86FuncId(bxInstruction_c *instr){
 }
 void BX_CPP_AttrRegparmN(1) BX_CPU_C::JMP_Jd(bxInstruction_c *i)
 {
-  logXv6X86FuncId(i);
+  logFnEnt(i);
   Bit32u new_EIP = EIP + (Bit32s) i->Id();
   branch_near32(new_EIP);
   BX_INSTR_UCNEAR_BRANCH(BX_CPU_ID, BX_INSTR_IS_JMP, PREV_RIP, new_EIP);
