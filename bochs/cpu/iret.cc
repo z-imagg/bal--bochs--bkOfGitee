3bd3994c26ed6ec74f107a537a6d9419453590e7
prgrmz07 <prgrmz07@163.com>
2024-01-16 11:57:59 +0800

    iret_protected__同级返、iret_protected__外级返：增加original_eip

--- bochs/cpu/iret.cc
+++ bochs/cpu/iret.cc
@@ -179,13 +179,14 @@ BX_CPU_C::iret_protected(bxInstruction_c *i)
     }
 #endif
 
+  Bit32u original_eip=(EIP);
     /* load CS-cache with new code segment descriptor */
     branch_far(&cs_selector, &cs_descriptor, new_eip, cs_selector.rpl);
     //记录一条日志, 日志行的字段们 : cs选择子 cs_selector 、 代码段描述符 cs_descriptor、 新eip new_eip
     std::string cs_selector_json_text=BX_CPU_THIS -> selector_json_text("cs_selt",&cs_selector);
     std::string cs_descriptor_json_text=BX_CPU_THIS -> descriptor_json_text("cs_desc",&cs_descriptor);
     //csv日志=IRET中断返回指令函数模拟内的共用逻辑:iret_protected:@情况2_同权级中断返回;cs_selector_json_text;cs_descriptor_json_text
-    BX_INFO(("~L~=返断程共逻:iret_protected__同级返:; %s; %s; 0x%x",cs_selector_json_text.c_str(), cs_descriptor_json_text.c_str(), new_eip));
+    BX_INFO(("~L~=返断程共逻:iret_protected__同级返:; %s; %s; 0x%x; 0x%x",cs_selector_json_text.c_str(), cs_descriptor_json_text.c_str(), original_eip,new_eip));
 
     /* top 6/12 bytes on stack must be within limits, else #SS(0) */
     /* satisfied above */
@@ -317,6 +318,7 @@ BX_CPU_C::iret_protected(bxInstruction_c *i)
     }
 #endif
 
+  Bit32u original_eip=(EIP);
     /* load CS:EIP from stack */
     /* load the CS-cache with CS descriptor */
     /* set CPL to the RPL of the return CS selector */
@@ -346,7 +348,7 @@ BX_CPU_C::iret_protected(bxInstruction_c *i)
     函数分类:函数名:函数功能解释@在函数内的区域
     */
     //csv日志=IRET中断返回指令函数模拟内的共用逻辑:iret_protected:@情况3_外权级中断返回;cs_selector_json_text;cs_descriptor_json_text;new_eip;ss_selector_json_text;ss_descriptor_json_text
-    BX_INFO(("~L~=返断程共逻:iret_protected__外级返:; %s; %s; 0x%x; %s; %s",cs_selector_json_text.c_str(), cs_descriptor_json_text.c_str(), new_eip, ss_selector_json_text.c_str(),ss_descriptor_json_text.c_str()));
+    BX_INFO(("~L~=返断程共逻:iret_protected__外级返:; %s; %s; 0x%x; 0x%x; %s; %s",cs_selector_json_text.c_str(), cs_descriptor_json_text.c_str(), original_eip,new_eip, ss_selector_json_text.c_str(),ss_descriptor_json_text.c_str()));
 
     if (ss_descriptor.u.segment.d_b)
       ESP = new_esp;
